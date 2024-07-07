afad1a209fa6a82b5582442f04fdcbd193e546a5
JohnBach <hoangbachtran2016@gmail.com>
2024-07-07 09:21:05 +0700

    random vec3s

--- src/Random.h
+++ src/Random.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "src/pch/pch.h"
+#include "src/Utils.h"
 #include <random>
 
 class Random
@@ -13,5 +14,28 @@ public:
     inline static float Float()
     {
         return m_distribution(m_generator);
+    }
+
+    inline static float Float(float min, float max)
+    {
+        return min + (max - min) * Float();
+    }
+
+    inline static Vec3 Vector3()
+    {
+        return Vec3(
+            Random::Float(),
+            Random::Float(),
+            Random::Float()
+        );
+    }
+
+    inline static Vec3 Vector3(float min, float max)
+    {
+        return Vec3(
+            Random::Float(min, max),
+            Random::Float(min, max),
+            Random::Float(min, max)
+        );
     }
 };
